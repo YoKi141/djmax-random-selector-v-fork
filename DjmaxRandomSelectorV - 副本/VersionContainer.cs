f6789d3a3fc9ecb5a5c27f42bfa8a5e54ac206d3
YoKi141 <yoki14176@gmail.com>
2026-02-20 15:56:45 +0800

    Add files via upload

---
+++ DjmaxRandomSelectorV - 副本/VersionContainer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DjmaxRandomSelectorV
+{
+    public class VersionContainer
+    {
+        public Version CurrentAppVersion { get; set; }
+        public Version LatestAppVersion { get; set; }
+        public long AllTrackVersion { get; set; }
+        public string AppdataVersion { get; set; }
+    }
+}
