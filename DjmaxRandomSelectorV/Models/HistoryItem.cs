ee54db1811a2257cadb4764265cd635d847f48d3
YoKi141 <yoki14176@gmail.com>
2026-02-20 16:03:38 +0800

    Add files via upload

---
+++ DjmaxRandomSelectorV/Models/HistoryItem.cs
@@ -0,0 +1,17 @@
+﻿using Dmrsv.RandomSelector;
+
+namespace DjmaxRandomSelectorV.Models
+{
+    public class HistoryItem
+    {
+        public int Number { get; set; }
+        public MusicInfo Info { get; set; }
+        public string Category { get; set; }
+        public string Style { get; set; }
+        public string Level { get; set; }
+        public string Time { get; set; }
+
+        public string ButtonTunes => Style[..2];
+        public string Difficulty => Style[2..4];
+    }
+}
