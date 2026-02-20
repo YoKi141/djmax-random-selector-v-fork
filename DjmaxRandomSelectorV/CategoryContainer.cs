456202b35777f78e03c1f7b284ec9f034a402061
YoKi141 <yoki14176@gmail.com>
2026-02-20 16:00:08 +0800

    Add files via upload

---
+++ DjmaxRandomSelectorV/CategoryContainer.cs
@@ -0,0 +1,24 @@
+﻿using DjmaxRandomSelectorV.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DjmaxRandomSelectorV
+{
+    public class CategoryContainer
+    {
+        private List<Category> _categories;
+
+        public List<Category> GetCategories()
+        {
+            return _categories.ConvertAll(x => x);
+        }
+
+        public void SetCategories(Dmrsv3AppData appData)
+        {
+            var categories = appData.Categories.Where(cat => cat.Type != 3);
+            var plis = appData.PliCategories
+                              .SelectMany(pli => pli.Minors, (pli, m) => new Category(m.Name, $"{pli.Major}:{m.Name}", null, 3));
+            _categories = new List<Category>(categories.Union(plis));
+        }
+    }
+}
---
+++ DjmaxRandomSelectorV/FileManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DjmaxRandomSelectorV
+{
+    public interface IFileManager
+    {
+        T Import<T>(string path);
+        void Export<T>(T instance, string path);
+        Task<string> RequestAsync(string url);
+        void Write(string content, string path);
+    }
+
+    public class FileManager : IFileManager
+    {
+        public T Import<T>(string path)
+        {
+            using var reader = new StreamReader(path);
+            string json = reader.ReadToEnd();
+            var option = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+            T instance = JsonSerializer.Deserialize<T>(json, option);
+
+            return instance ?? throw new NullReferenceException();
+        }
+
+        public void Export<T>(T instance, string path)
+        {
+            var option = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+            string json = JsonSerializer.Serialize(instance, option);
+            Write(json, path);
+        }
+
+        public async Task<string> RequestAsync(string url)
+        {
+            using var client = new HttpClient();
+            string result = await client.GetStringAsync(url);
+            return result;
+        }
+
+        public void Write(string content, string path)
+        {
+            using var writer = new StreamWriter(path);
+            writer.Write(content);
+        }
+    }
+}
